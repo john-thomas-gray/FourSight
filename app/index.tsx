88f9ee4c94661701f82b033f53923cfdd90c48ee
John <john.thom.gray@gmail.com>
2025-06-14 19:07:22 -0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import '../global.css';
+
+export default function Index() {
+  return (
+    <View
+      className="border-2 border-red"
+    >
+      <Text className="text-red-100 font-bold text-bold">Edit appfdsfdsindex.tsx to edit this screen.</Text>
+    </View>
+  );
+}
